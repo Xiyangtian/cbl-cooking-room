d350e86812e84b3f1851fce82ced021797e0a4d4
Xiyangtian <87839982+Xiyangtian@users.noreply.github.com>
2023-07-10 11:22:00 +0800

    更新 pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
 game.pushScene()
 
 scene_util.captureRegisteringRoomScenes(() => {
-//    mushroomfarm.prepare_mushroomfarm()
+    test01.prepare_test01()
+    game.popScene()
+})
+
+scene_util.captureRegisteringRoomScenes(() => {
+    test02.prepare_test02()
     game.popScene()
 })
 
